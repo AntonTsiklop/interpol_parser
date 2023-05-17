cd5c575c5829d58b7d44be846d5c6b00332d220f
AntonTsiklop <tsikalo.tsikalo@mail.ru>
2023-05-17 13:15:50 +0300

    class extension

--- notice_parser.py
+++ notice_parser.py
@@ -2,9 +2,9 @@
 Данные получены с помощью Interpol Notices API https://interpol.api.bund.dev.
 Перебор по фамилиям A-Z + перебор по именам A-Z, если запрос включает более 160 элементов,
 то добавляется фильтр по возрасту: 18-30, 30-40, 40-50, 50-70, 70-90, 90-110.
-Метод parse_notices сохраняет данные со страницы View-Red-Notices в текущей рабочей директории, метод
-parse_details помимо данных со страницы сохраняет подробные данные об объекте, метод parse_details_with_thumbnails
-помимо перечисленного выше сохраняет миниатюры в папке images, остальные методы вспомогательные.
+Класс NoticesParser служит для получения данных со страниц, классы NoticesDetailsParser
+и NoticesThumbnailsParser расширяют функционал класса NoticeParser для получения подробной
+информации об объектах и миниатюр.
 """
 
 from string import ascii_uppercase
@@ -13,7 +13,7 @@ import json
 import os
 
 
-class NoticeParser:
+class NoticesParser:
 
     age_max_list = [30, 40, 50, 70, 90, 110]
     age_min_list = [18, 30, 40, 50, 70, 90]
@@ -21,7 +21,6 @@ class NoticeParser:
     def __init__(self, link: str):
         self.link = link
         self.data_list = []             # информация об объектах
-        self.data_list_detail = []      # подробная информация об объектах
         self.num_of_res = 0             # кол-во результатов
 
     @staticmethod
@@ -36,16 +35,8 @@ class NoticeParser:
             f.write(']\n')
             f.close()
 
-    def query(self, char_name: str, char_forename: str, age_max=None, age_min=None):
-        params = dict(resultPerPage=200, name=f'^{char_name}', forename=f'^{char_forename}', ageMax=age_max,
-                      ageMin=age_min)
-        request = requests.get(self.link, params)
-        query = request.json()
-        total = int(query['total'])                 # кол-во результатов
-        data = query['_embedded']['notices']        # словарь, в котором лежит информация об объекте
-        return data, total
-
-    def save_images(self, data, image_dir: str):
+    @staticmethod
+    def save_images(data, image_dir: str):
         for el in data:
             try:
                 image_request = requests.get(el['_links']['thumbnail']['href']) # тут лежит миниатюра, но она есть
@@ -57,10 +48,17 @@ class NoticeParser:
                 out.close()
             except KeyError:
                 pass
-            detail_request = requests.get(el['_links']['self']['href'])         # тут лежит подробная информация
-            self.data_list_detail.append(detail_request.json())                 # об объекте
 
-    def parse_notices(self, filename: str):
+    def query(self, char_name: str, char_forename: str, age_max=None, age_min=None):
+        params = dict(resultPerPage=200, name=f'^{char_name}', forename=f'^{char_forename}', ageMax=age_max,
+                      ageMin=age_min)
+        request = requests.get(self.link, params)
+        query = request.json()
+        total = int(query['total'])                 # кол-во результатов
+        data = query['_embedded']['notices']        # словарь, в котором лежит информация об объекте
+        return data, total
+
+    def parse_notices(self):
         for char_name in ascii_uppercase:
             for char_forename in ascii_uppercase:
                 data, total = self.query(char_name, char_forename)
@@ -77,60 +75,35 @@ class NoticeParser:
                 self.num_of_res += total
                 print(f'Found {self.num_of_res} notices')
                 self.data_list.append(data)
-        self.json_write(self.data_list, f'{filename}')
-
-    def parse_details(self, filename_notices: str, filename_notices_details: str):
-        for char_name in ascii_uppercase:
-            for char_forename in ascii_uppercase:
-                data, total = self.query(char_name, char_forename)
-                if total == 0:
-                    continue
-                if total > 160:
-                    for age_min, age_max in zip(self.age_min_list, self.age_max_list):
-                        data, total = self.query(char_name, char_forename, age_max=age_max, age_min=age_min)
-                        if total == 0:
-                            continue
-                        self.num_of_res += total
-                        for el in data:
-                            detail_request = requests.get(el['_links']['self']['href'])
-                            self.data_list_detail.append(detail_request.json())
-                        self.data_list.append(data)
-                    continue
-                self.num_of_res += total
-                print(f'Found {self.num_of_res} notices')
-                for el in data:
-                    detail_request = requests.get(el['_links']['self']['href'])
-                    self.data_list_detail.append(detail_request.json())
-                self.data_list.append(data)
-        self.json_write(self.data_list, f'{filename_notices}')
-        self.json_write(self.data_list_detail, f'{filename_notices_details}')
-
-    def parse_details_with_thumbnails(self, filename_notices: str, filename_notices_details: str, image_dir: str):
-        for char_name in ascii_uppercase:
-            for char_forename in ascii_uppercase:
-                data, total = self.query(char_name, char_forename)
-                if total == 0:
-                    continue
-                if total > 160:
-                    for age_min, age_max in zip(self.age_min_list, self.age_max_list):
-                        data, total = self.query(char_name, char_forename, age_max=age_max, age_min=age_min)
-                        if total == 0:
-                            continue
-                        self.num_of_res += total
-                        self.save_images(data, image_dir)
-                        self.data_list.append(data)
-                    continue
-                self.num_of_res += total
-                print(f'Found {self.num_of_res} notices')
-                self.save_images(data, image_dir)
-                self.data_list.append(data)
-        self.json_write(self.data_list, f'{filename_notices}')
-        self.json_write(self.data_list_detail, f'{filename_notices_details}')
+        return self.data_list
 
 
-red_notices = NoticeParser('https://ws-public.interpol.int/notices/v1/red')
-# red_notices.parse_notices('results')
-# red_notices.parse_details('results', 'results_details')
-red_notices.parse_details_with_thumbnails('results', 'results_details', 'images')
-yellow_notices = NoticeParser('https://ws-public.interpol.int/notices/v1/yellow')
-yellow_notices.parse_details_with_thumbnails('results_y', 'results_details_y', 'images')
+class NoticeDetailsParser(NoticesParser):
+
+    def __init__(self, link: str):
+        super().__init__(link)
+        self.data_list_detail = []  # подробная информация об объектах
+
+    def parse_details(self, data_list: list):
+        if not data_list:
+            self.data_list = super().parse_notices()
+        else:
+            self.data_list = data_list
+        for data in self.data_list:
+            for el in data:
+                detail_request = requests.get(el['_links']['self']['href'])
+                self.data_list_detail.append(detail_request.json())
+        return self.data_list_detail
+
+
+class NoticesThumbnailsParser(NoticesParser):
+
+    def parse_thumbnails(self, image_dir: str, data_list: list):
+        if not data_list:
+            self.data_list = super().parse_notices()
+        else:
+            self.data_list = data_list
+        for data in self.data_list:
+            self.save_images(data, image_dir)
+
+
