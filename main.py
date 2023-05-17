97ff625770ffc0ec9749bbded778fc19020599d4
AntonTsiklop <tsikalo.tsikalo@mail.ru>
2023-05-17 13:28:03 +0300

    class extension

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ red_notices_thumbnails.parse_thumbnails('images_red', red_notices_list)
 yellow_notices_link = 'https://ws-public.interpol.int/notices/v1/yellow'
 yellow_notices = NoticesParser(yellow_notices_link)
 yellow_notices_list = yellow_notices.parse_notices()
-yellow_notices.json_write(yellow_notices_list, 'red_notices')
+yellow_notices.json_write(yellow_notices_list, 'yellow_notices')
 
 yellow_notices_details = NoticeDetailsParser(yellow_notices_link)
-yellow_notices_details.json_write(yellow_notices_details.parse_details(yellow_notices_list), 'red_notices_details')
+yellow_notices_details.json_write(yellow_notices_details.parse_details(yellow_notices_list), 'yellow_notices_details')
 
 yellow_notices_thumbnails = NoticesThumbnailsParser(yellow_notices_link)
 yellow_notices_thumbnails.parse_thumbnails('images_yellow', yellow_notices_list)
